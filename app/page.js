89fb81a00a1d98ec32e68dd773b99a45446a399e
Anthropohpobe <anthropophobe@MacBook-Air-de-Anthropohpobe.local>
2023-06-08 17:05:47 +0200

    fetching data

--- app/page.js
+++ app/page.js
@@ -2,29 +2,25 @@ import React from "react";
 import ItemCard from "./src/components/ItemCard";
 import React, { useState, useEffect } from 'react';
 
-
-
 export default function Home() {
   const [data, setData] = useState(null);
+
   useEffect(() => {
-    // Fonction de récupération des données depuis l'API externe
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:8097/sakilamdb/products');
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error);
+        console.log(error);
       }
     };
 
-    fetchData(); // Appel de la fonction fetchData lors du montage du composant
+    fetchData(); 
 
-    // Nettoyage : fonction exécutée lors du démontage du composant
     return () => {
-      // Effectuer des actions de nettoyage si nécessaire
     };
-  }, []); // [] signifie que useEffect s'exécutera uniquement une fois, lors du montage initial
+  }, []);
 
   return (
     <div className='h-full'>
